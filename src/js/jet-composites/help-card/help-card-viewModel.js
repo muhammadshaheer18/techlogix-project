e293c48643b5977eafca176d7f593da5b7d6a41b
Muhammad Shaheer <119890345+muhammadshaheer18@users.noreply.github.com>
2025-10-15 14:49:41 +0500

    HelpCardStyleUpdate

--- src/js/jet-composites/help-card/help-card-viewModel.js
+++ src/js/jet-composites/help-card/help-card-viewModel.js
@@ -7,12 +7,12 @@ define(['knockout'], function (ko) {
       {
         image: "/css/images/Icons/nic.svg",
         title: "Find your CNIC Identity Number",
-        text: "Permanent citizens of Pakistan can find their CNIC number on their NADRA-issued identity card."
+        text: "Permanent citizens of Pakistan can find their CNIC number on their identity card issued by NADRA."
       },
       {
         image: "/css/images/Icons/cheque.png",
-        title: "Locate Your Cheque Number",
-        text: "The cheque number is printed at the top-right corner of your bank cheque."
+        title: "How to find your Account Number",
+        text: "The Simplest way of finding your Account Number is to look for it on your cheque book."
       },
     ]);
 
